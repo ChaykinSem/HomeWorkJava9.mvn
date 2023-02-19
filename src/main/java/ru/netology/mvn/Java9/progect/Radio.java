cc7f619ad1daeea0ac8a6d523e0213276527a470
Semen Vitalich <Chaykin96@gmail.com>
2023-02-19 20:26:10 +0300

    Final Progect

---
+++ src/main/java/ru/netology/mvn/Java9/progect/Radio.java
@@ -0,0 +1,79 @@
+package ru.netology.mvn.Java9.progect;
+public class Radio {
+    private int radioNumber;
+    private int soundVolume;
+
+    // Настройка Радио
+    public int getRadioNumber() {
+        return radioNumber;
+    }
+
+    public void  setRadioNumber(int newRadioNumber) {
+        if (newRadioNumber < 0) {
+            return;
+        }
+        if (newRadioNumber > 9) {
+            return;
+        }
+        radioNumber = newRadioNumber;
+    }
+
+    public void setToMaxNumber() {
+        radioNumber = 9;
+    }
+
+    public int nextRadioNumber() {
+        if (radioNumber >= 9) {
+            radioNumber = 0;
+        } else {
+            radioNumber = radioNumber + 1;
+        }
+        return radioNumber;
+    }
+
+    public int prevRadioNumber() {
+        if (radioNumber <= 0) {
+            radioNumber = 9;
+        } else {
+            radioNumber = radioNumber - 1;
+        }
+        return radioNumber;
+    }
+
+    // Настройка Звука
+    public int getSoundVolume() {
+        return soundVolume;
+    }
+
+    public void setSoundVolume(int newSoundVolume) {
+        if (newSoundVolume < 0) {
+            return;
+        }
+        if (newSoundVolume > 10) {
+            return;
+        }
+        soundVolume = newSoundVolume;
+    }
+
+    public void setToMaxVolume() {
+        soundVolume = 10;
+    }
+
+    public int nextSoundVolume() {
+        if (soundVolume >= 10) {
+            soundVolume = 10;
+        } else {
+            soundVolume = soundVolume + 1;
+        }
+        return soundVolume;
+    }
+
+    public int prevSoundVolume() {
+        if (soundVolume <= 0) {
+            soundVolume = 0;
+        } else {
+            soundVolume = soundVolume - 1;
+        }
+        return soundVolume;
+    }
+}
